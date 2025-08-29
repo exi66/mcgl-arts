753157737e4185aa451893b2df64befc0e95d2c6
exi66 <eternityexi@gmail.com>
2025-08-29 20:20:52 +0400

    prevent inline assets and keep original names

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,14 @@ const __dirname = dirname(__filename);
 export default defineConfig({
   base: "/mcgl-arts/",
   plugins: [react()],
+  build: {
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        assetFileNames: "[name].[ext]",
+      },
+    },
+  },
   resolve: {
     alias: {
       "@arts": path.resolve(__dirname, "src/assets/arts"),
